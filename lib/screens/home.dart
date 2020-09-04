f17724da81534719451f4132ccf3bc0d66a30ff1
ShraddhaMhatree <shradz2526@gmail.com>
2020-09-04 04:31:17 -0700

    Ui changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text("Breweries"),
         centerTitle: true,
       ),
       body: SafeArea(
